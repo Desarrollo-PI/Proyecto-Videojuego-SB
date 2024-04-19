b583444b94bd75d7748010d1876ff36298e2b0c0
Nicolas Herrera <herreran903@gmail.com>
2024-04-18 23:22:54 -0500

    Update environment and lights settings

--- src/game/globals/Lights.jsx
+++ src/game/globals/Lights.jsx
@@ -1,22 +1,28 @@
-import { Color } from "three";
+import React from 'react';
+import { ambientLight, directionalLight, Color } from 'three';
 
 const Lights = () => {
-    return <>
-        <ambientLight
-            intensity={0.5}
-        />
-        <directionalLight
-            castShadow={true}
-            position={[2, 10, 0]}
-            color={new Color("#FFF700")}
-            intensity={2}
-            shadow-mapSize = {[2048, 2048]}
-            shadow-camera-far = {50}
-            shadow-camera-left = {-10}
-            shadow-camera-right = {10}
-            shadow-camera-top = {10}
-            shadow-camera-bottom = {-10}
-        />
-    </>
+    return (
+        <>
+            {/* Luz ambiental */}
+            <ambientLight
+                intensity={0.0000000000000000001} // Reduce la intensidad de la luz ambiental
+            />
+            {/* Luz direccional */}
+            <directionalLight
+                castShadow={true}
+                position={[2, 10, 0]}
+                color={new Color("#FFF700")}
+                intensity={0.000001} // Reduce la intensidad de la luz direccional
+                shadow-mapSize={[2048, 2048]}
+                shadow-camera-far={50}
+                shadow-camera-left={-10}
+                shadow-camera-right={10}
+                shadow-camera-top={10}
+                shadow-camera-bottom={-10}
+            />
+        </>
+    );
 }
+
 export default Lights;
