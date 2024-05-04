c3a4d9c2608379edd3d48b378ac429f90cb2226b
Nicolas Herrera <herreran903@gmail.com>
2024-05-04 13:29:19 -0500

    Remove debug mode from Physics component

--- src/game/pages/layout/LayoutLevel.jsx
+++ src/game/pages/layout/LayoutLevel.jsx
@@ -237,7 +237,7 @@ const LayoutLevel = () => {
             </Float>
             <StormEnvironment {...chooseProps()} />
             <OrbitControls />
-            <Physics debug={true}>
+            <Physics>
               <Outlet />
               <Ecctrl
                 camInitDis={-3}
