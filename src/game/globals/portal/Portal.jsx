07e87eb114df1c3d318b8849f00726c5af9ba14b
Samuel Galindo <itzsaky@outlook.com>
2024-06-02 16:51:42 -0500

    implementacion portal

---
+++ src/game/globals/portal/Portal.jsx
@@ -0,0 +1,52 @@
+
+import React, { useRef } from 'react'
+import { useGLTF } from '@react-three/drei'
+import { RigidBody } from '@react-three/rapier'
+
+const Portal = (props) => {
+  const { nodes, materials } = useGLTF('/assets/models/elements/Portal.glb')
+  const refPortal = useRef()
+
+  const onEnterCollisionPortal = (e) => {
+    if (e.rigidBodyObject.name === 'playerBody') {
+      console.log('Enter collision with player')
+    }
+  }
+
+  return (
+    <RigidBody
+    colliders="cuboid"
+    type="fixed"
+    onCollisionEnter={onEnterCollisionPortal}
+
+    ref={refPortal}
+
+    {...props}
+  >
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Object_3002.geometry}
+        material={materials['Material.012']}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Object_3002_1.geometry}
+        material={materials['Material.013']}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Object_3002_2.geometry}
+        material={materials['Material.014']}
+      />
+    </group>
+  </RigidBody>
+  )
+}
+
+export default Portal
+
+useGLTF.preload('/Portal.glb')
---
+++ src/game/pages/levelThree/elements/Portal.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import Portal from '../../../globals/portal/Portal'
+
+
+const Portals = () => {
+    return (
+        <>
+        <Portal position={[11.2, 0.3, -30]}/>
+        </>
+    )
+    }
+
+export default Portals
