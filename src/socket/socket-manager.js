482a3fea591bb097226c5c056c85c06fadb7a27e
John <john.freddy.belalcazar@gmail.com>
2024-06-15 11:10:45 -0500

    Configure deploy server

--- src/socket/socket-manager.js
+++ src/socket/socket-manager.js
@@ -2,7 +2,8 @@
 
 import socketIOClient from "socket.io-client"
 
-export const socket = socketIOClient("http://localhost:5000")
+// export const socket = socketIOClient("http://localhost:5000")
+export const socket = socketIOClient("https://proyecto-videojuego-sb-sockets.onrender.com")
 
 export const disconnectSocket = () => {
 	console.log('Disconnecting socket...')
