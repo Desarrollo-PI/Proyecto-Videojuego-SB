2f7a9600e82668c93cc95c62a335513cb359f840
John <john.freddy.belalcazar@gmail.com>
2024-06-14 21:21:07 -0500

    Fix conflicts

--- src/game/pages/levelFour/LevelFourPage.jsx
+++ src/game/pages/levelFour/LevelFourPage.jsx
@@ -1,16 +1,13 @@
 import WorldLevelFourWithPhysisc from './World'
 import { EvilWizard } from '../../globals/enemies/evilWizard/EvilWizard'
 import Lights from './Lights'
-<<<<<<< HEAD
 import { socket } from '../../../socket/socket-manager'
 import { useEffect } from 'react'
 import { SecondPlayer } from '../../globals/player/SecondPlayer'
 import { usePlayer } from '../../../providers/player/PlayerProvider'
 import { Skeleton } from '../../globals/enemies/skeleton/Skeleton'
-=======
 import Collectibles from './collectibles/Collectibles'
 import Checkpoints from './checkpoints/Checkpoints'
->>>>>>> c6a7e9f1b795d5143756bdb606ab3d73406a1019
 
 const LevelFourPage = () => {
 
@@ -37,24 +34,9 @@ const LevelFourPage = () => {
     <>
       {/* <Lights /> */}
       <WorldLevelFourWithPhysisc />
-<<<<<<< HEAD
-      <Skeleton
-          idEnemy={1}
-          position={[10, 5, -20]}
-          action={'Walk'}
-          takeLife={null}
-          deathEnemy={null}
-          isPlayerDeath={false}
-          speed={3}
-          isPlaying={null}
-        />
-      <EvilWizard position={[0, -1, -48]} action={0} />
-      <SecondPlayer position={[0,0,0]} />
-=======
       <Collectibles />
       <Checkpoints />
       <EvilWizard position={[0, 5, -48]} action={0} />
->>>>>>> c6a7e9f1b795d5143756bdb606ab3d73406a1019
     </>
   )
 }
